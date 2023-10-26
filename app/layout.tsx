14c30faf76917f2c3776eb58981e4a7599e82cd3
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-26 18:28:26 +0530

    NOTION_CLONE : Completed Marketing Section and Integrated Convex with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/providers/convex-provide";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -31,10 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            storageKey="notion-clone-theme-2"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
   );
 }
