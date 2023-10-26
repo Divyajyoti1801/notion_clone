d6ff8f41af690a49351763e552a42cc77c3c6936
Divyajyoti <Divyajyoti182000@gmail.com>
2023-10-26 11:38:50 +0530

    NOTION_CLONE : Project-Setup & Authentication Completed

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Notion Clone",
+  description:
+    "A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you and your team.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
